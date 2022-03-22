dadea8c03630db596c588c2f2735d53008392e47
JacquelynSH <elpatchaembroidery@gmail.com>
2022-03-22 11:23:40 -0700

    hello.js

---
+++ hello.js
@@ -0,0 +1,15 @@
+// 1st example.
+
+/*const sayHello  = function () {
+  console.log("Hello, frenchfries");
+} 
+sayHello();
+// output - Hello, world
+*/
+// next example
+const sayHello = function (name) {
+  console.log("Hello, " + name);
+}
+sayHello("caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
